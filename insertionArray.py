270d50e564e9baf4733fad59569416427391e8e0
Abhishek <abhisheksharma7669@gmail.com>
2024-10-14 11:21:39 +0530

    Array Insertion Practice

---
+++ insertionArray.py
@@ -0,0 +1,17 @@
+from array import *
+
+np = array('i', [1,2,3,4,5])
+
+print(np)
+
+np.insert(0,6)   # insert in the front
+
+print(np)   
+
+np.insert(2,7)   # insert in the middle
+
+print(np)   
+
+np.append(8)   # insert in the end
+
+print(np)   
